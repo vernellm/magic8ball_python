b4865b07c19553540327d109dc9b48e76de52f6d
vernellm <vtmangum@gmail.com>
2025-06-09 12:43:12 -0500

    initialize random number generator

--- magic8.py
+++ magic8.py
@@ -2,8 +2,20 @@
 #
 # Description: Simple python program that is essentially a Magic 8 Ball
 
+import random
+
+# Initial variable set up
+
 name = "Vernell"
 
 question = "Am I the best looking man of all time?"
 
 answer = ""
+
+# Initializing random number generator
+
+random_number = random.randint(1, 9)
+
+# Testing for random numbers
+# print(random_number)
+
