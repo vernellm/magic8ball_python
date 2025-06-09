5db6649babbfcf9f3713509f33e2cfcb3f971996
vernellm <vtmangum@gmail.com>
2025-06-09 12:38:53 -0500

    initial variable setup

---
+++ magic8.py
@@ -0,0 +1,9 @@
+# Author: Vernell Mangum
+#
+# Description: Simple python program that is essentially a Magic 8 Ball
+
+name = "Vernell"
+
+question = "Am I the best looking man of all time?"
+
+answer = ""
